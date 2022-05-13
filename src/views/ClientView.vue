74b1cf67efdaee6730e2cb3c5f07c5e4ff4a719d
Sky Infinity <dylan.hautecoeur@gmail.com>
2022-05-13 15:44:11 +0200

    Add field to form

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="page about-page">
     <h1>Client n°{{ client.clientNumber }}</h1>
-    <div class="box">
-      <div class="info-list">
-        <form id="client-infos" action="" method="post">
-          <div class="field-groups">
-            <FieldComponent type="text" name="nom" value="Doe"/>
-            <FieldComponent type="text" name="prénom" value="John"/>
-          </div>
-          <FieldComponent type="date" name="birthdate" text="Date de naissance" :value="client.birthdate"/>
-          <FieldComponent type="email" name="email" text="Adresse e-mail" :value="client.email" disabled/>
-          <FieldComponent type="number" name="number-of-account" text="Nombre de compte" :value="client.numberOfAccount.toString()" disabled/>
-          <FieldComponent type="tel" name="phone" text="Numéro de téléphone" :value="client.phone"/>
-          <FieldComponent type="password" name="password" text="Mot de passe"/>
-          <ButtonComponent iconClass="ri-edit-line">Modifier les informations</ButtonComponent>
-        </form>
-      </div>
-      <div class="actions">
+    <div class="box-contents">
+      <!--  Informations  -->
+      <div class="box informations">
+        <div class="info-list">
+          <form id="client-infos" action="" method="post">
+            <div class="field-groups">
+              <FieldComponent type="text" name="last-name" value="Doe" text="Nom"/>
+              <FieldComponent type="text" name="first-name" value="John" text="Prénom"/>
+            </div>
+            <FieldComponent type="date" name="birthdate" text="Date de naissance" :value="client.birthdate" disabled/>
+            <div class="field-groups">
+              <FieldComponent type="email" name="email" text="Adresse e-mail" :value="client.email" disabled/>
+              <FieldComponent type="tel" name="phone" text="Numéro de téléphone" :value="client.phone"/>
+            </div>
+            <FieldComponent type="text" name="address" text="Adresse" :value="client.address"/>
+            <FieldComponent type="text" name="city" text="Ville" :value="client.city"/>
+            <FieldComponent type="text" name="country" text="Pays" :value="client.country"/>
 
+            <ButtonComponent iconClass="ri-edit-line">Modifier les informations</ButtonComponent>
+          </form>
+        </div>
+        <div class="actions">
+
+        </div>
+      </div>
+      <!--  Contact  -->
+      <div class="box contact">
+        <a :href="`tel:${client.phone}`" class="tel btn-contact" >Appeler <i class="ri-phone-line"></i></a>
+        <a :href="`mailto:${client.email}`" class="mail btn-contact">Envoyer un message <i class="ri-mail-line"></i></a>
       </div>
     </div>
   </div>
@@ -36,17 +48,17 @@ export default {
   data() {
     return {
       client: {
-        id: 6,
-        title: 'Mr',
-        clientNumber: 12679976475,
-        firstName: 'Emmanuel',
-        lastName: 'Macron',
-        birthdate: '08/10/1984',
-        email: 'user@example.com',
-        phone: '0656994236',
-        address: '37 rue de la victoire',
-        city: 'Lille',
-        country: 'France',
+        id: 6, // ok
+        title: 'Mr', // ok
+        clientNumber: 12679976475, // ok
+        firstName: 'Emmanuel', // ok
+        lastName: 'Macron', // ok
+        birthdate: '08/10/1984', // ok
+        email: 'user@example.com', // ok
+        phone: '0656994236', // ok
+        address: '37 rue de la victoire', // ok
+        city: 'Lille', // ok
+        country: 'France', // ok
         numberOfAccount: 2
       }
     }
@@ -63,5 +75,40 @@ export default {
     display: flex;
     flex-direction: column;
     gap: 16px;
+  }
+  .box-contents {
+    display: grid;
+    grid-template-areas: "info contact";
+    gap: 32px;
+  }
+  .informations {
+    grid-area: info;
+  }
+  .contact {
+    grid-area: contact;
+    display: flex;
+    gap: 16px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .btn-contact {
+      padding: 16px;
+      border-radius: 8px;
+      width: 100%;
+      max-width: 368px;
+      justify-content: center;
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      transition: var(--a-transition);
+      background-color: var(--c-primary);
+      color: var(--c-secondary);
+
+      &:hover {
+        background-color: var(--c-tertiary);
+      }
+    }
   }
 </style>
